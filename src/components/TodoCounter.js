4d1d9919b85af511b7d97ec90fd5a0509abca9be
mario robayo <marioferyhwh@gmail.com>
2024-10-24 16:53:36 -0500

    curalse 05 en proceso

--- src/components/TodoCounter.js
+++ src/components/TodoCounter.js
@@ -1,7 +1,14 @@
+const style = {
+  fontSize: "24px",
+  textAlign: "center",
+  margin: "0",
+  padding: "48px",
+  fontWeight: "bold",
+};
 function TodoCouter(props) {
   const { completed, total } = props;
   return (
-    <h1>
+    <h1 style={style}>
       Has Completado {completed} de {total}
     </h1>
   );
